04d82c909b1c292c3f6feca0fb66e2ac811409e5
K1risama <arkornushin76@mail.ru>
2026-03-18 15:11:54 +0500

    fixed

--- practice6/directory_management/move_files.py
+++ practice6/directory_management/move_files.py
@@ -1,5 +1,8 @@
 import shutil
+import os
 
-shutil.copy("file.txt", "backup/file.txt")
+os.makedirs(r"C:\Users\arkor\Documents\PP2_repo\practice6\directory_management\backup", exist_ok=True)
+os.makedirs(r"C:\Users\arkor\Documents\PP2_repo\practice6\directory_management\archhive", exist_ok=True)
 
-shutil.move("file.txt", "archive/file.txt")
+shutil.copy(r"C:\Users\arkor\Documents\PP2_repo\practice6\directory_management\file.txt", r"C:\Users\arkor\Documents\PP2_repo\practice6\directory_management\backup\file.txt")
+shutil.move(r"C:\Users\arkor\Documents\PP2_repo\practice6\directory_management\file.txt", r"C:\Users\arkor\Documents\PP2_repo\practice6\directory_management\archhive\file.txt")
